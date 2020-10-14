6752d7252925a7b4e5ac3bcb05405068eadde855
MariaJoseGarPaz <70005553+MariaJoseGarPaz@users.noreply.github.com>
2020-10-13 22:21:58 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,11 @@
-from Bio.Seq import Seq
 from Bio import SeqIO
 from Bio.SeqFeature import FeatureLocation, SeqFeature
-from Bio.SeqRecord import record
+from Bio.SeqRecord import SeqRecord
 def summarize_contents(filename):
-         
-        record =SeqIO.read (filename,"genbank")
-        print(len(record.features))
-sumarize_contents(filename)
+         record = SeqIO.read(filename, "genbank")
+         print(record.name)
+         records = list(SeqIO.parse(filename, "genbank"))
+         print("num_records = %i records" % len(records))
+         for seq_record in SeqIO.parse(filename,"genbank"):
+           print(record.id)
+summarize_contents(filename)
