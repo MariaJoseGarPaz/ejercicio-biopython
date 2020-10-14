4c1da3689950a0887e1227dd22858ae56193f44f
MariaJoseGarPaz <70005553+MariaJoseGarPaz@users.noreply.github.com>
2020-10-13 23:04:18 -0700

    Update script.py

--- script.py
+++ script.py
@@ -3,10 +3,12 @@ from Bio.SeqFeature import FeatureLocation, SeqFeature
 from Bio.SeqRecord import SeqRecord
 def summarize_contents(filename):
         record = SeqIO.read(filename, "genbank")
-        print(record.name)
+        print("Name: ", record.name)
+        ##Aqui debe ir lo de la ruta (path)
         records = list(SeqIO.parse(filename, "genbank"))
         print("num_records = %i records" % len(records))
-        
+
         for seq_record in SeqIO.parse(filename,"genbank"):
-                print(record.id)
+                print("ID:",record.id)
+                ##Aqui debe ir lo de location
 summarize_contents(filename)
