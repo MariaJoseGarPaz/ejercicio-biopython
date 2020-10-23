20fefbadf570689ee25b6a675222e238bc8fdaf6
María José García Paz <70005553+MariaJoseGarPaz@users.noreply.github.com>
2020-10-22 21:23:55 -0700

    Update script.py

--- script.py
+++ script.py
@@ -2,22 +2,22 @@ from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 import os
 import sys
-filename= "/mnt/c/Users/alma_/Desktop/bioinfo/ls_orchid.gbk"
+filename= "/mnt/c/Users/alma_/Desktop/ejercicio-biopython/data/ls_orchid.gbk"
 def summarize_contents(filename):
-        AllRecords=[]
         FileList = []
         FileList = os.path.split(filename)
-        print("File : ", FileList[1])
+        cadena = " "
+        cadena=("File: " + FileList[1])
         records = list(SeqIO.parse(filename, "genbank"))
         path=  os.path.dirname(filename)
-        print ("Path: ", path)
-        print("num_records = %i records" % len(records))
+        cadena+= ("\nPath: " + path)
+        cadena+= ("\nnum_records= "+ str(len(records)))
         print("\n\n")
         for seq_record in SeqIO.parse(filename, "genbank"):
-                AllRecords.append(seq_record.name)
-             
-                print("-ID: ", seq_record.id)
-                print("-Name: ", seq_record.name)
-                print("-Description: ", seq_record.description)
-                print("\n")
-summarize_contents(filename)
+                cadena+= ("\n-ID: " + str(seq_record.id))
+                cadena+=("\nName: " + str(seq_record.name))
+                cadena+=("\nDescription: " + str(seq_record.description))
+                cadena+= ("\n")
+        return cadena
+result=summarize_contents(filename)
+print(result)
