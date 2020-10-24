b68e193c58cc48ad3967d6ed53305be4f13cde05
María José García Paz <70005553+MariaJoseGarPaz@users.noreply.github.com>
2020-10-23 23:47:09 -0700

    Update script.py

--- script.py
+++ script.py
@@ -5,19 +5,32 @@ import sys
 filename= "/mnt/c/Users/alma_/Desktop/ejercicio-biopython/data/ls_orchid.gbk"
 def summarize_contents(filename):
         FileList = []
+        File_Extension = []
+
         FileList = os.path.split(filename)
+        File_Extension = os.path.splitext(filename)
+
+        if(File_Extension[1] == ".gbk"):
+                type_file= "genbank"
+        else:
+                type_file= "fasta"
+                pass
+
+        records = list(SeqIO.parse(filename, type_file))
         cadena = " "
-        cadena=("File: " + FileList[1])
-        records = list(SeqIO.parse(filename, "genbank"))
         path=  os.path.dirname(filename)
+
+        cadena=("File: " + FileList[1])
         cadena+= ("\nPath: " + path)
         cadena+= ("\nnum_records= "+ str(len(records)))
-        print("\n\n")
-        for seq_record in SeqIO.parse(filename, "genbank"):
+
+        for seq_record in SeqIO.parse(filename, type_file):
                 cadena+= ("\n-ID: " + str(seq_record.id))
                 cadena+=("\nName: " + str(seq_record.name))
                 cadena+=("\nDescription: " + str(seq_record.description))
-                cadena+= ("\n")
         return cadena
-result=summarize_contents(filename)
-print(result)
+
+if __name__ == "__main__":
+        resultado = summarize_contents(filename)
+        print(resultado)
+
