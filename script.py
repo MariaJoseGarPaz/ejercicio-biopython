b929208b609f83732e8063a15e71f7fb80fe5879
MariaJoseGarPaz <mariagarciapaz@hotmail.com>
2020-11-10 13:48:22 -0700

    Se agrego la funcion print_proteins_and_codons_using_mitocondrial_yeast_table() la cual dada una secuencia, devuelve un diccionario con el RNAm, las proteinas traducidas y los codones de paro según la tabla de la mitocondria de levadura
    [biopython-tarea2-ex5]

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ from Bio.Seq import Seq
 from Bio.Data import CodonTable
 import os
 import sys
+
 filename= os.path.abspath("data/NC_002703.gbk")
 seq1= "attAg"
 seq2= "ggct"
@@ -45,7 +46,6 @@ def concatenate_and_get_reverse_of_complement(sequence_a, sequence_b):
         reverse = concatenated.reverse_complement()
         return reverse.upper()
 def print_protein_and_codons_using_standard_table(sequence):
-
 
         #Conversión de string a Seq
         DNAsequence= Seq(sequence)
@@ -97,8 +97,63 @@ def print_protein_and_codons_using_standard_table(sequence):
                 
         
         return diccionario
-                                
+
+def print_proteins_and_codons_using_mitocondrial_yeast_table(sequence):
+
+        #Conversión de string a Seq
+        DNAsequence= Seq(sequence)
+
+        
+        diccionario={}
+
+        #Definición de las keys del diccionario 
+        diccionario['mRNA']= DNAsequence.transcribe().upper()
+        diccionario['proteins'] = []
+        diccionario['stop_codons'] = []
+        
+        #Traducción usando la tabla mitocondria de la levadura
+        Amino_Acid = DNAsequence.translate(table= "Yeast Mitochondrial")
+        i = 0
+
+        #Ciclo que recorre la secuencia buscando codones de inicio y de paro para añadirlo a la lista proteins
+        while i < (len(DNAsequence)):
+                if (DNAsequence[i*3:i*3+3].upper() == "ATA") or (DNAsequence[i*3:i*3+3].upper() == "ATG") or (DNAsequence[i*3:i*3+3].upper() == "GTG"):
+                        
+                        if i+1 == len(DNAsequence):
+                                diccionario['proteins'].append(Amino_Acid[i:])
+                                break
+                        j = i+1
+                        while j < (len(DNAsequence)):
+                                if(DNAsequence[j*3:j*3+3] == "TAG" ) or (DNAsequence[j*3:j*3+3] == "TAA")  :
+                                        diccionario['proteins'].append(Amino_Acid[i:j])
+                                        diccionario['stop_codons'].append(DNAsequence[j*3:j*3+3])
+                                        i = j
+                                        break
+                                j= j+1
+                #SI NO HAY CODON DE INICIO Y CODON DE PARO NO SE AGREGA A LA LISTA
+                if((DNAsequence[i*3:i*3+3].upper() == "ATA") or (DNAsequence[i*3:i*3+3].upper() == "ATG") or (DNAsequence[i*3:i*3+3].upper() == "GTG")) and ((DNAsequence[j*3:j*3+3] == "TAG" ) or (DNAsequence[j*3:j*3+3] == "TAA")) :
+                        diccionario['proteins'].append(Amino_Acid[i:j])
+                        break
+                i= i+1
+                
+                       
+        
+        
+                        
+
+        #Si la lista esta vacía, no hay codones de paro
+        if diccionario['stop_codons'] == []:
+                diccionario['stop_codons'] = "Not stop codons were found."   
+
+        #Si la lista esta vacía, no hay proteínas
+        if diccionario['proteins'] == []:
+                diccionario['proteins'] = "Not proteins were found."
+                
+        
+        return diccionario              
 
 if __name__ == "__main__":
         proteins = print_protein_and_codons_using_standard_table(sequence)
         print(proteins)
+        mitocondrial = print_proteins_and_codons_using_mitocondrial_yeast_table(sequence)
+        print(mitocondrial)
